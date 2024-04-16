eb9e498c8304cde463a982c7d678bbbfc2a5c2a9
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-04-16 22:55:14 +0700

    Implement unsubscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -11,5 +11,13 @@ pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::subscribe(product_type) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
-    }
+    };
+}
+
+#[get("/unsubscribe/<product_type>")]
+pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
+    return match NotificationService::unsubscribe(product_type) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
 }
